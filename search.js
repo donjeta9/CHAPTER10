0d3426176829193b4e5bc5957ebc3f94a6aa20b9
donjeta9 <57753983+donjeta9@users.noreply.github.com>
2020-02-09 00:07:37 +0100

    Create search.js

---
+++ search.js
@@ -0,0 +1,44 @@
+var a  = new Array ( 100 );  // create an array
+
+  // fill array with integer values from 0 to 198
+
+  for( var i = 0; i < a.length; ++i )
+  {
+  	a[ i ] = 2 * i;
+  }  // end for
+
+    // function called when "search"  button is pressed
+
+    function buttonPressed()
+    {
+    	// get the input text field
+    	var inputVal = document.getElementById( "inputVal" );
+
+    	// get the result paragraph
+    	var result = document.getElementById( "result" );
+
+    	// get the search key from the input text field then perform the search
+    	var searchKey = parseInt( inputVal.value );
+    	var element = a.indexOf( searchKey ); 
+    	 if ( element != -1 )
+    	 {
+    	 	result.inner.HTML = "found value in element " + element;
+
+    	 }  // end if
+    	 else
+    	 {
+    	 	result.inner.HTML = "value not found";
+    	 }  // end else
+    
+
+    }      // end functionPressed
+
+          // register searchButton's click event handler
+          function start()
+          {
+          	var searchButton = document.getElementById( "searchButton" );
+          	searchButton.addEventListener( "click" , buttonPressed, false );
+
+          }  // end function start
+
+          window.addEventListener( "load", start , false );
